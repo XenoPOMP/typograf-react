14501fa7f38ea9f26200213f3b7d8970b58ee43d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-13 19:20:07 +0300

    🪝 write hook for formatter

--- src/lib/hooks/useTypograf.ts
+++ src/lib/hooks/useTypograf.ts
@@ -1,3 +1,14 @@
-export const useTypograf = () => {
-  return {};
+import { useMemo } from 'react';
+import TypografFormatter from 'typograf';
+
+/**
+ * This hook creates memoized formatter for text
+ * and returns it.
+ *
+ * @param options
+ */
+export const useTypograf = (
+  ...options: ConstructorParameters<typeof TypografFormatter>
+): TypografFormatter => {
+  return useMemo(() => new TypografFormatter(...options), [options]);
 };
